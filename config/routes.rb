f75645ae0b34ca998bda338d085f98495813c58e
qyao <qyao4@academic.rrc.ca>
2024-02-07 13:49:40 -0600

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,20 @@
+Rails.application.routes.draw do
+  root "articles#index"
+  get "/articles", to: "articles#index"
+  get "/articles/new", to: "articles#new", as: :new_article
+  post "/articles", to: "articles#create"
+  get "/articles/:id", to: "articles#show", as: :article
+  get '/articles/:id/edit', to: 'articles#edit', as: :edit_article
+  put '/articles/:id', to: 'articles#update'
+  patch '/articles/:id', to: 'articles#update'
+  delete '/articles/:id', to: 'articles#destroy', as: :delete_article
+
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  # Defines the root path route ("/")
+  # root "posts#index"
+end
